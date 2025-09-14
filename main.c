fabebc7d234868f9946d95c499f223724fabd6cc
Bjarnedolle <christoffer.m.rodholm@gmail.com>
2025-09-14 14:37:05 +0200

    1

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int motoramount() {
+    int motors;
+    printf("Enter the number of motors: ");
+    scanf("%d", motors);
+    return motors;
+}
+
+float Packageweight() {
+    float weight;
+    printf("Enter the package weight in pounds: ");
+    scanf("%f", weight);
+    return weight;
+}
+
+int main() {
+    int motors = motoramount();
+    float weight = Packageweight();
+
+    if ((float)weight / motors <= 5.6)
+        printf("Yes! The conveyor can carry the package.");
+    else
+        printf("No! The conveyor cannot carry the package.");
+
+    return 0;
+}
