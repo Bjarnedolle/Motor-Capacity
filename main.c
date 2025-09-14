7b8f7329a7de343ea7b5f67aabd7cf50f3baeb8c
Bjarnedolle <christoffer.m.rodholm@gmail.com>
2025-09-14 15:03:30 +0200

    2

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int motoramount() {
     int motors;
     printf("Enter the number of motors: ");
-    scanf("%d", motors);
+    scanf("%d", &motors);
     return motors;
 }
 
 float Packageweight() {
     float weight;
     printf("Enter the package weight in pounds: ");
-    scanf("%f", weight);
+    scanf("%f", &weight);
     return weight;
 }
 
